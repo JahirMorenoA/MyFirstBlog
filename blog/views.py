6686918bc73be804dc03ff826bd1b2b9732df1f0
Jahir Moreno <jahir.quantum8.3@gmail.com>
2018-12-24 20:27:23 -0600

    creando la clase Post en models.py y creando BD con migrations

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from .models import Post
+
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Jahir Moreno', 
-        'title': 'Blog Post 1', 
-        'content': 'First Post content',
-        'date_posted': 'December 21, 2018'
-    }, 
-    {
-        'author': 'Iris Nabil', 
-        'title': 'Blog Post 2', 
-        'content': 'Second Post content',
-        'date_posted': 'December 22, 2018'
-    }
-]
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
         }
     return render(request, 'blog/home.html', context)
 
